172e87b1724798c2117e345a7adb51083e66bd20
tomashchuk <tomashchukdmytro@gmail.com>
2023-02-15 01:14:41 +0200

    add base model

--- sites/models.py
+++ sites/models.py
@@ -1,7 +1,9 @@
 from django.contrib.gis.db import models
 from django_countries.fields import CountryField
 
+from core.models import BaseModel
 
-class Company(models.Model):
+
+class Company(BaseModel):
     name = models.CharField(max_length=100)
     country = CountryField()
