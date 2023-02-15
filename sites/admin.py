68d0e1688f5bfa9f6c4489a18593fa749009ee4d
tomashchuk <tomashchukdmytro@gmail.com>
2023-02-15 14:26:05 +0200

    add world border relation to site and register site on admin side

--- sites/admin.py
+++ sites/admin.py
@@ -1,5 +1,6 @@
 from django.contrib.gis import admin
-from .models import WorldBorder, Company
+from .models import WorldBorder, Company, Site
 
 admin.site.register(Company, admin.ModelAdmin)
 admin.site.register(WorldBorder, admin.OSMGeoAdmin)
+admin.site.register(Site, admin.OSMGeoAdmin)
--- sites/models.py
+++ sites/models.py
@@ -9,28 +9,6 @@ class Company(BaseModel):
     country = CountryField()
 
 
-class Site(BaseModel):
-
-    class Status(models.TextChoices):
-        TODO = 'todo', 'To Do'
-        IN_PROGRESS = 'in_progress', 'In Progress'
-        READY_FOR_REVIEW = 'ready_for_review', 'Ready For Review'
-        DONE = 'done', 'Done'
-        FROZEN = 'frozen', 'Frozen'
-
-    created_by = models.ForeignKey(get_user_model(), on_delete=models.PROTECT)
-    company = models.ForeignKey(Company, on_delete=models.SET_NULL, null=True)
-
-    position = models.PointField(srid=4326)
-    mpoly = models.MultiPolygonField(srid=4326, null=True)
-
-    status = models.CharField(
-        max_length=30,
-        choices=Status.choices,
-        default=Status.TODO,
-    )
-
-
 # Regular GeoDjango WorldBorder model. Filled from helpers.data
 class WorldBorder(BaseModel):
     fips = models.CharField(max_length=2, null=True)
@@ -51,3 +29,27 @@ class WorldBorder(BaseModel):
 
     def __str__(self):
         return self.name
+
+
+class Site(BaseModel):
+
+    class Status(models.TextChoices):
+        TODO = 'todo', 'To Do'
+        IN_PROGRESS = 'in_progress', 'In Progress'
+        READY_FOR_REVIEW = 'ready_for_review', 'Ready For Review'
+        DONE = 'done', 'Done'
+        FROZEN = 'frozen', 'Frozen'
+
+    created_by = models.ForeignKey(get_user_model(), on_delete=models.PROTECT)
+    company = models.ForeignKey(Company, on_delete=models.SET_NULL, null=True)
+    country = models.ForeignKey(WorldBorder, on_delete=models.PROTECT)
+
+    position = models.PointField(srid=4326)
+    mpoly = models.MultiPolygonField(srid=4326, null=True)
+
+    status = models.CharField(
+        max_length=30,
+        choices=Status.choices,
+        default=Status.TODO,
+    )
+
